c31e271bc0efdce1f4f85c9b52d8bb3a01e71945
Andres Recalde (JHU) <arecald1@jhu.edu>
2021-02-22 03:48:14 -0500

    Added heroku functions and deployed

--- src/main/java/WebServer.java
+++ src/main/java/WebServer.java
@@ -16,6 +16,7 @@ public class WebServer {
         final String KEY = System.getenv("SIS_API_KEY");
         Unirest.config().defaultBaseUrl("https://sis.jhu.edu/api");
 
+        port(getHerokuAssignedPort());
         staticFiles.location("/public");
 
         get("/", (req, res) -> {
@@ -39,8 +40,16 @@ public class WebServer {
             return null;
         }, new HandlebarsTemplateEngine());
 
-
-
-
     }
+
+    private static int getHerokuAssignedPort() {
+        // Heroku stores port number as an environment variable
+        String herokuPort = System.getenv("PORT");
+        if (herokuPort != null) {
+            return Integer.parseInt(herokuPort);
+        }
+        //return default port if heroku-port isn't set (i.e. on localhost)
+        return 4567;
+    }
+
 }
